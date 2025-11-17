971a21d294b274b36eb5661111926c1029a9084c
Erwinn7 <ahissouannselme14@gmail.com>
2025-11-17 17:46:16 +0100

    mise a jour vue inutile

--- sql/Partie3/CreateRole.sql
+++ sql/Partie3/CreateRole.sql
@@ -16,26 +16,8 @@ BEGIN
 END
 $$;
 
--- Vue des chercheurs et de leurs laboratoires
-
-CREATE OR REPLACE VIEW vue_chercheurs_laboratoires AS
-SELECT c.id AS chercheur_id, c.first_name, c.last_name, c.email,
-       l.name AS laboratoire, i.name AS institution
-FROM chercheur c
-LEFT JOIN laboratoire l ON c.labo_id = l.id
-LEFT JOIN institution i ON l.institution_id = i.id;
 
 
--- Vue des projets avec leur responsable et labo
-
-CREATE OR REPLACE VIEW vue_projets_complets AS
-SELECT p.id AS projet_id, p.title, p.discipline, p.budget,
-       c.first_name || ' ' || c.last_name AS responsable,
-       l.name AS laboratoire, i.name AS institution
-FROM projet p
-JOIN chercheur c ON p.responsable_id = c.id
-JOIN laboratoire l ON p.laboratoire_id = l.id
-JOIN institution i ON l.institution_id = i.id;
 
 
 
